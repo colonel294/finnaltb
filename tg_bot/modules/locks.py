22e4c0bf2db82a0b41384d801d058bc3d5647004
colonel294 <48035763+colonel294@users.noreply.github.com>
2019-04-12 06:20:51 +0430

    Update locks.py

--- tg_bot/modules/locks.py
+++ tg_bot/modules/locks.py
@@ -34,7 +34,7 @@ LOCK_TYPES = {'استیکر': Filters.sticker,
               }
 
 GIF = Filters.animation
-OTHER = Filters.game | Filters.sticker | GIF | new_chat_members
+OTHER = Filters.game | Filters.sticker | GIF 
 MEDIA = Filters.audio | Filters.document | Filters.video | Filters.video_note | Filters.voice | Filters.photo
 MESSAGES = Filters.text | Filters.contact | Filters.location | Filters.venue | Filters.command | MEDIA | OTHER
 PREVIEWS = Filters.entity("url")
